34223c91822199adbd4c6b3349325c267ae6fc49
Oben <144821542+xxobenxx@users.noreply.github.com>
2023-12-11 15:55:09 +0100

    FP
    footer update

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -4,10 +4,10 @@ import React from 'react'
 const Footer = ()=> <footer>
     <ul>
 <li>El Turco Services </li>
-<li>All rights reserved 2023.</li>
-<li>T : +34 600 793 638</li>
-<li>@: kerken[at]gmail</li>
-<li>L : Foret 18, 08019 BCN</li>
+<li>All rights reserved 2023. </li>
+<li>T : +34 600 793 638 </li>
+<li>@: kerken[at]gmail </li>
+<li>L : Foret 18, 08019 BCN </li>
 </ul>
 
 </footer>
