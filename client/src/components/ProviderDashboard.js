9a155977b8d7685f1b991d90b283872542f069e6
Oben <144821542+xxobenxx@users.noreply.github.com>
2023-12-11 14:57:37 +0100

    finalProject

--- client/src/components/ProviderDashboard.js
+++ client/src/components/ProviderDashboard.js
@@ -90,7 +90,7 @@ const handleSubmit = async (e) => {
       {/* Provider Form */}
       <div align="center">
       <form onSubmit={handleSubmit} >
-      {updated && <p style={{color: 'red'}}>Updated successfully!</p>}
+      {updated && <div style={{color: 'red'}}>Updated successfully!</div>}
 
       <h1>PROVIDER INFORMATION</h1>
         <label>Name:</label>
