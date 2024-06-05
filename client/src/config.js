5205fd92b3878e59cbddeeff87aeb4abc9800ecc
Oben <144821542+xxobenxx@users.noreply.github.com>
2024-06-05 21:22:17 +0200

    Set up for Heroku deployment

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // export const URL = "http://localhost:4001";
 // export const URL = "https://book-a-petsitter-demo.surge.sh/"
-export const URL = "https://book-a-pet-sitter-fe34d0002614.herokuapp.com/";
+export const URL = "https://book-a-pet-sitter-fe34d0002614.herokuapp.com";
